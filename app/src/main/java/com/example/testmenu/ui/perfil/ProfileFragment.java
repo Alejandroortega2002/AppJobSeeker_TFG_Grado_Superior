816422f0569e21519c06d05dc668afac19d818a4
Alejandro Ortega Núñez <alexaon.ao@gmail.com>
2023-04-19 07:09:20 +0200

    Alejandro Ortega: Transferencia de datos  al perfil y pequeños retoques

--- app/src/main/java/com/example/testmenu/ui/perfil/ProfileFragment.java
+++ app/src/main/java/com/example/testmenu/ui/perfil/ProfileFragment.java
@@ -4,18 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.testmenu.databinding.FragmentPerfilBinding;
+import com.example.testmenu.ui.perfil.ProfileViewModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class ProfileFragment extends Fragment {
 
     /*Declaramos su Xml correspondiente a traves del ViewBiding*/
     private FragmentPerfilBinding binding;
+    private TextView telefono, nombreU, email;
+    private FirebaseAuth mAuth;
+    //FirestoreDataBase
+    private FirebaseFirestore mFirestore;
+    private String idUser;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,8 +42,32 @@ public class ProfileFragment extends Fragment {
         binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        telefono = (TextView) binding.nTelefono;
+        nombreU = (TextView) binding.nPerfil;
+        email = (TextView) binding.pEmail;
+
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseUser user = mAuth.getCurrentUser();
+        mFirestore = FirebaseFirestore.getInstance();
+        idUser = mAuth.getCurrentUser().getUid();
+
+        reyenarInformacionUsuario();
 
         return root;
+
+    }
+
+    public void reyenarInformacionUsuario(){
+        DocumentReference documentReference = mFirestore.collection("Usuarios").document(idUser);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                nombreU.setText(value.getString("usuario"));
+                email.setText(value.getString("email"));
+                telefono.setText(value.getString("telefono"));
+
+            }
+        });
     }
 
     @Override
