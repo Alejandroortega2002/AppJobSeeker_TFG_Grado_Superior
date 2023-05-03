6bf1865d90f48699ae7f512473ee91fc74e25ff3
jefferson.mogollon <jefferson.universida17@gmail.com>
2023-05-03 17:56:47 +0200

    pendiente de hacer el activity_user_profile.xml y UserProfileActivity.java!

---
+++ app/src/main/java/com/example/testmenu/activities/UserProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.testmenu.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.testmenu.R;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
